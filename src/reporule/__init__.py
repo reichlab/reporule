cf4083c59700f00e4569e29d4ec320f09318f013
Becky Sweger <rsweger@umass.edu>
2025-04-27 09:57:34 -0400

    Get the top-level directory of the code
    This will probably work most of the time

--- src/reporule/__init__.py
+++ src/reporule/__init__.py
@@ -1,5 +1,12 @@
 """reporule initialization."""
 
 import os
+from importlib.util import find_spec
+from pathlib import Path
 
 TOKEN = os.environ.get("GITHUB_TOKEN", "")
+
+if find_spec("reporule") is not None:
+    REPORULE_PATH = Path(find_spec("reporule").origin).parent  # type: ignore
+else:
+    REPORULE_PATH = Path(__file__).parent.parent
